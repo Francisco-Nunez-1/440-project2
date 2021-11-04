bca1a801039ee44c6218536d76b09805d53ef4b4
cmchen4431 <89759311+cmchen4431@users.noreply.github.com>
2021-11-04 02:36:52 -0700

    Code to push user inputted data to the database
    Issue:
    --can't push data from
       create_account_both.ui to the
       database

--- main.py
+++ main.py
@@ -11,9 +11,12 @@
 import sys
 from PyQt5.uic import loadUi
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QDialog, QApplication, QWidget, QStackedWidget, QLabel
+from PyQt5.QtWidgets import QDialog, QApplication, QWidget, QStackedWidget, QLabel, QComboBox, QLineEdit
+from PyQt5 import *
 import sqlite3
 import webbrowser
+import mysql.connector
+from mysql.connector import Error
 
 
 # Show the welcome_screen, created class that will have objects,
@@ -83,11 +86,12 @@ class LoginScreen(QDialog):
 # *********************************** END OF NEEDS WORK DONE TO IT **********************************************
 # ***************************************************************************************************************
 
-def nextpage_function():
-    # this will open the SecondCreateAccountScreen window in the current window by calling the .ui class
-    second_page_create_account = SecondCreateAccountScreen()
-    widget.addWidget(second_page_create_account)
-    widget.setCurrentIndex(widget.currentIndex() + 1)
+# def nextpage_function():
+#     # this will open the SecondCreateAccountScreen window in the current window by calling the .ui class
+#     second_page_create_account = SecondCreateAccountScreen()
+#     widget.addWidget(second_page_create_account)
+#     widget.setCurrentIndex(widget.currentIndex() + 1)
+
 
     # # extracting the user and password
     # typed_fname = self.fname_textbx.text()
@@ -109,7 +113,7 @@ def nextpage_function():
 
 
 class CreateAccountScreen(QDialog):
-    def __init__(self):
+    def __init__(self, parent=None):
         super(CreateAccountScreen, self).__init__()
         # load the gui to our python code
         loadUi("create_account_both.ui", self)
@@ -119,14 +123,31 @@ class CreateAccountScreen(QDialog):
         self.confirmPassword_textbx.setEchoMode(QtWidgets.QLineEdit.Password)
 
         # when next button is clicked it will go to nextpage_funtion
-        self.nextbtn.clicked.connect(nextpage_function)
+        self.nextbtn.clicked.connect(self.nextpage_function)
+
+    def nextpage_function(self):
+        # this will open the SecondCreateAccountScreen window in the current window by calling the .ui class
+
+        # firstname = self.fname_textbx.text()
+        # print(firstname)
+
+        # Get user input from create_account_both.ui
+        fname = self.fname_textbx.text()
+        lname = self.lname_textbx.text()
+        userdata = (fname, lname)
+        print(userdata)
+
+        second_page_create_account = SecondCreateAccountScreen()
+        widget.addWidget(second_page_create_account)
+        widget.setCurrentIndex(widget.currentIndex() + 1)
 
 
 # this is for link
-class HyperlinkLabel(QDialog):
+class HyperlinkLabel(QLabel):
+    # def __init__(self, parent=None):
     def __init__(self, parent=None):
-        super().__init__()
-        self.setStyleSheet('font-size: 35px')
+        super(HyperlinkLabel, self).__init__()
+        # self.setStyleSheet('font-size: 35px')
         self.setOpenExternalLinks(True)
         self.setParent(parent)
 
@@ -134,6 +155,7 @@ class HyperlinkLabel(QDialog):
 class SecondCreateAccountScreen(QDialog):
     def __init__(self):
         super(SecondCreateAccountScreen, self).__init__()
+
         # load the gui to our python code
         loadUi("second_create_account.ui", self)
 
@@ -141,8 +163,105 @@ class SecondCreateAccountScreen(QDialog):
         # https://learndataanalysis.org/create-hyperlinks-pyqt5-tutorial/
         linkTemplate = '<a href={0}>{1}</a>'
 
-        # linklbl = HyperlinkLabel(self)
-        self.linklbl.setText(linkTemplate.format('https://Google.com', 'Google.com'))
+        linklbl = HyperlinkLabel(self)
+        # self.linklbl.setText(linkTemplate.format('https://google.com', 'Google.com'))
+        self.linklbl.setText(linkTemplate.format('https://google.com', 'Google.com'))
+
+        self.createAccountbtn.clicked.connect(self.connectdatabase)
+
+    def connectdatabase(self):
+        try:
+            # Gets input from the form
+            # Only able to get input from second_create_account.ui
+
+            # From create_account_both.ui
+            # Ends the program when ran
+            # fname = self.fname_textbx.text()
+            # print(fname)
+
+            # From second_create_account.ui
+            department = self.department_comboBox.currentText()
+            print(department)
+
+            connection = mysql.connector.connect(host='107.180.1.16',
+                                                 database='cis440fall2021group1',
+                                                 user='fall2021group1',
+                                                 password='fall2021group1')
+
+            if connection.is_connected():
+                db_info = connection.get_server_info()
+                print("Connected to MySQL Server version ", db_info)
+                cursor = connection.cursor()
+                cursor.execute("select database();")
+                record = cursor.fetchone()
+                print("You're connected to database: ", record)
+
+
+                # # Insert data to the database
+                # Only work with inputs from second_create_account.ui
+                # sql = "INSERT INTO Employees (FName, Department) VALUES (%s, %s)"
+                # val = (fname, department)
+                # cursor.execute(sql, val)
+                # connection.commit()
+                #
+                # print(cursor.rowcount, "record inserted.")
+
+        except Error as e:
+            print("Error while connecting to MySQL", e)
+        finally:
+            if connection.is_connected():
+                cursor.close()
+                connection.close()
+                print("MySQL connection is closed")
+
+
+
+# Connect to a mysql database (hard coded data)
+# class ConnectDatabase:
+#     def __init__(self):
+#         try:
+#             connection = mysql.connector.connect(host='107.180.1.16',
+#                                                  database='cis440fall2021group1',
+#                                                  user='fall2021group1',
+#                                                  password='fall2021group1')
+#
+#             if connection.is_connected():
+#                 db_info = connection.get_server_info()
+#                 print("Connected to MySQL Server version ", db_info)
+#                 cursor = connection.cursor()
+#                 cursor.execute("select database();")
+#                 record = cursor.fetchone()
+#                 print("You're connected to database: ", record)
+#
+#                 # # Insert data to the database
+#                 # sql = "INSERT INTO Employees (FName, LName, AdvisingRole, Department) VALUES (%s, %s, %s, %s)"
+#                 # val = ("John", "Doe", "Mentee", "IT")
+#                 # cursor.execute(sql, val)
+#                 # connection.commit()
+#                 #
+#                 # print(cursor.rowcount, "record inserted.")
+#
+#                 # Insert data to the database
+#                 department
+#
+#                 sql = "INSERT INTO Employees (Department) VALUES (%s)"
+#                 val = department
+#                 cursor.execute(sql, val)
+#                 connection.commit()
+#
+#                 print(cursor.rowcount, "record inserted.")
+#
+#
+#         except Error as e:
+#             print("Error while connecting to MySQL", e)
+#         finally:
+#             if connection.is_connected():
+#                 cursor.close()
+#                 connection.close()
+#                 print("MySQL connection is closed")
+
+
+
 
 
 # main
